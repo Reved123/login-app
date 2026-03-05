1333e0cc5ee99392899d8b4191a5fdfc495e9b4e
Reved123 <revanthnani90533@gmail.com>
2026-03-05 13:17:24 +0530

    Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Welcome from "./Welcome";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/welcome" element={<Welcome />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- frontend/src/Welcome.js
+++ frontend/src/Welcome.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-function Welcome() {
-  const username = localStorage.getItem("username");
-
+const Welcome = () => {
+  const username = localStorage.getItem("username") || "User";
   return (
-    <div>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontFamily: "Arial, sans-serif"
+    }}>
       <h1>Welcome, {username}!</h1>
+      <p>You have successfully logged in.</p>
     </div>
   );
-}
+};
 
 export default Welcome;
