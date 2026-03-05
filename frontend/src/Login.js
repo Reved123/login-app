88a27928cb4bf8c4128171cb73d169dc76f60349
Reved123 <revanthnani90533@gmail.com>
2026-03-05 11:26:47 +0530

    Frontend

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -20,7 +20,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:5000/login", {
+      const response = await axios.post("https://login-backend-51mx.onrender.com", {
         username,
         password,
       });
